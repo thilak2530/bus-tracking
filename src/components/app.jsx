db3edafa4bced53e537fdd55e747526933b2d02f
Thilak Gaddam <thilakgaddam@Thilaks-MacBook-Air.local>
2025-06-05 14:24:45 +0530

    changes in app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Login from "./login";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route ,Navigate} from "react-router-dom";
 import Home from "./home";
 import Setting from "./setting";
 import Bus_timing from "./bus-timig";
+import { info, otherbusinfo,busTimings } from "./info";
 
-function App() {
-    return (
-        <Router>
+
+
+function App(){
+    return(
+        <Router basename="/bus-tracking">
             <Routes>
                 <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<Home />} />
+                <Route path="/home" element={<Home />} /> 
                 <Route path="/setting" element={<Setting />} />
                 <Route path="/home/bus-timings" element={<Bus_timing />} />
             </Routes>
         </Router>
-    );
+    );  
 }
 
 export default App;
